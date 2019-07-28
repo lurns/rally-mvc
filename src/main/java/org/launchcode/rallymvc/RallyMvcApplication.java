8ff22c058a8630a3e7bff3e5a77bac05f4552435
Lurns <lrnlee08@gmail.com>
2019-07-28 12:48:32 -0500

    initial commit

---
+++ src/main/java/org/launchcode/rallymvc/RallyMvcApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.rallymvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RallyMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RallyMvcApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/launchcode/rallymvc/controllers/UserController.java
@@ -0,0 +1,17 @@
+package org.launchcode.rallymvc.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("")
+    public String index(Model model) {
+        model.addAttribute("title", "Sign up for Rally");
+        return "index";
+    }
+
+}
