330418a19e83de334156599d2fddc0a8472992aa
Lurns <lrnlee08@gmail.com>
2019-07-30 20:37:53 -0500

    implemented basic login and session storage

--- src/main/java/org/launchcode/rallymvc/controllers/UserController.java
+++ src/main/java/org/launchcode/rallymvc/controllers/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 @Controller
@@ -20,7 +22,7 @@ public class UserController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping("")
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model) {
         User newUser = new User();
         model.addAttribute("user", newUser);
@@ -29,24 +31,39 @@ public class UserController {
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public String processRegistration(@ModelAttribute @Valid User newUser, Errors errors, Model model, @RequestParam String verifyPass) {
+    public String processRegistration(@ModelAttribute @Valid User newUser, Errors errors,
+                                      Model model, @RequestParam String verifyPass,
+                                      HttpServletRequest request) {
 
+        //check for duplicate email
         boolean exists = false;
         User dupEmailUser = userDao.findByEmail(newUser.getEmail());
         if (dupEmailUser != null) {
             exists = true;
-        }
-
-        if (errors.hasErrors() || !newUser.getPassword().equals(verifyPass)) {
-            model.addAttribute("title", "Sign up for Rally");
-            return "index";
-        } else if (exists) {
             model.addAttribute("duplicate", "Email associated with existing account.");
             model.addAttribute("title", "Sign up for Rally");
             return "index";
         }
 
+        //check for overall errors
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Sign up for Rally");
+            return "index";
+        }
+
+        //check passwords match
+        if (!newUser.getPassword().equals(verifyPass)) {
+            model.addAttribute("title", "Sign up for Rally");
+            model.addAttribute("verifyPass", "Passwords must match.");
+            return "index";
+        }
+
+        //pass validation, ready to create user and make session
         userDao.save(newUser);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("user", newUser);
+
         return "redirect:/dashboard";
     }
 
@@ -56,9 +73,31 @@ public class UserController {
         return "login";
     }
 
-    @RequestMapping(value = "dashboard", method = RequestMethod.GET)
-    public String dashboard(Model model, User user) {
-        model.addAttribute("title", "Hi, " + user.getNickname());
+    //TODO: user login
+    public String processLogin(Model model, @Valid User user, Errors errors,
+                               HttpServletRequest request) {
+
+
+        HttpSession session = request.getSession();
+        session.setAttribute("user", user);
+
+        return "redirect:/dashboard";
+
+    }
+
+    public String logout(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        return "index";
+    }
+
+    @RequestMapping(value="dashboard", method=RequestMethod.GET)
+    public String dashboard(Model model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        model.addAttribute("user", user);
         return "dashboard";
     }
 
