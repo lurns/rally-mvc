39beb257d6331517350c148a4e36e60962ac52c7
Lurns <lrnlee08@gmail.com>
2019-09-07 16:22:07 -0500

    change nickname in profile view

--- src/main/java/org/launchcode/rallymvc/controllers/UserProfileController.java
+++ src/main/java/org/launchcode/rallymvc/controllers/UserProfileController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import javax.validation.constraints.Size;
 
 @Controller
 public class UserProfileController {
@@ -36,7 +37,7 @@ public class UserProfileController {
 
     @RequestMapping(value = "profile/change-password", method = RequestMethod.POST)
     public String processChangePassword(@RequestParam int id, @RequestParam String currentpassword,
-                                        @RequestParam String newpassword, @RequestParam String retypepassword,
+                                        @RequestParam @Size(min=8) String newpassword, @RequestParam String retypepassword,
                                         HttpServletRequest request) {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
@@ -67,6 +68,25 @@ public class UserProfileController {
         if (user.getId() == id) {
             if (user.getPassword().equals(password) || BCrypt.checkpw(password, user.getPassword())) {
                 user.setEmail(newemail);
+                userDao.save(user);
+                return "redirect:/profile";
+            } else {
+                return "redirect:/dashboard";
+            }
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @RequestMapping(value = "profile/change-nickname", method = RequestMethod.POST)
+    public String processChangeNickname(@RequestParam @Size(min=3, max=15) String newnickname, @RequestParam String password,
+                                        @RequestParam int id, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        if (user.getId() == id) {
+            if (user.getPassword().equals(password) || BCrypt.checkpw(password, user.getPassword())) {
+                user.setNickname(newnickname);
                 userDao.save(user);
                 return "redirect:/profile";
             } else {
